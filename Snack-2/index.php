f3cbb741e5d15e2746a87f3128dd1d56b9bfb2b0
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-07-22 14:46:46 +0200

    working on Snack-2

--- Snack-2/index.php
+++ Snack-2/index.php
@@ -3,6 +3,26 @@
  che mail contenga un punto e una chiocciola e che age sia un numero. 
  Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
+<?php
+
+// Passo come parametri GET name, mail e age:
+$name = $_GET('name');
+$mail = $_GET('mail');
+$age = $_GET('age');
+
+// Definisco la variabile che conterrà il messaggio per l'utente:
+$userMessage;
+
+// Se name è più lungo di 3 caratteri (= count(name) > 3);
+// Se mail contiene un punto e una chiocciola (= strpos diverso da false);
+// Se mail contiene una chiocciola (= strpos diverso da false);
+if(count($name) > 3) && strpos($mail, '.') !== false && strpos($mail, '@') !== false {
+    $userMessage = 'Accesso Riuscito';
+} else {
+    $userMessage = 'Accesso Negato';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,6 +32,11 @@
     <title>Snack-2</title>
 </head>
 <body>
-    
+
+    <!-- Stampo nel DOM: -->
+    <h1>
+        <?php echo $userMessage; ?>
+    </h1>
+
 </body>
 </html>
